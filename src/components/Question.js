5889be372d0e1304bb7466287efdfb4a98b625b5
mudu-abdullahi <moxamudabdullahi2004@email.com>
2025-08-06 10:37:22 +0300

    Implement countdown timer and pass all tests

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      if (timeRemaining > 1) {
+        setTimeRemaining((prevTime) => prevTime - 1);
+      } else {
+        setTimeRemaining(10); // Reset timer
+        onAnswered(false);    // Time's up!
+      }
+    }, 1000);
+
+    // 👇 Match the test: clearTimeout should be called
+    return () => clearTimeout(timeoutId);
+  }, [timeRemaining, onAnswered]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
